5f8541e62ec0ac220339ea3ceaaa2e693b4d4ecf
Mehdi Khalili <me@mehdi-khalili.com>
2011-04-06 07:56:17 +1000

    Added a few types to the ProcessType for easier extension

--- Bddify/Core/ProcessType.cs
+++ Bddify/Core/ProcessType.cs
@@ -2,8 +2,11 @@
 {
     public enum ProcessType
     {
+        Firstly = 0,
         Execute = 1,
-        Report = 2,
-        HandleExceptions = 3
+        BeforeReport = 2,
+        Report = 3,
+        BeforeHandleException = 4,
+        HandleExceptions = 5
     }
 }
