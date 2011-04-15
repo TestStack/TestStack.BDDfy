52a6fe7745143f01b88c9b529252322da5d0c585
Mehdi Khalili <me@mehdi-khalili.com>
2011-04-15 18:11:18 +1000

    some updates on the nuget template files

--- NuGet/content/RunMe.cs.pp
+++ NuGet/content/RunMe.cs.pp
@@ -1,6 +1,5 @@
 using System;
 using Bddify.Core;
-using Bddify.Scanners;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace $rootnamespace$
@@ -90,22 +89,80 @@ namespace $rootnamespace$
         }
     }
 
-	[TestClass]
-    public class WhenTwoNumbersAreSubtracted
+	[Story(
+    AsA = "As a second grader",
+    IWant = "I want a calculator with four main functions",
+    SoThat = "So I do not have to learn to calculate!!")]
+    public class CalculatorCanDoFourMainMathFunctions
     {
-        [RunStepWithArgs(5, 3, 2)]
-        [RunStepWithArgs(1, 8, -7)]
-        [RunStepWithArgs(2, 3, 0)] // this should fail
-        void ThenTheResultShouldBeCorrect(int input1, int input2, int expectedResult)
-        {
-            if (input1 - input2 != expectedResult)
-                throw new Exception("Dude, subtract aint working");
-        }
-
         [TestMethod]
         public void Execute()
         {
             this.Bddify();
+        }
+    }
+
+	[RunScenarioWithArgs(1, 2, 3)]
+	[RunScenarioWithArgs(4, 5, 8)] // failing scenario
+    [WithStory(typeof(CalculatorCanDoFourMainMathFunctions))]
+	public class WhenTwoNumbersAreAdded
+	{
+		private int _expectedResult;
+	    private int _input1;
+	    private int _input2;
+
+	    void RunScenarioWithArgs(int input1, int input2, int expectedResult)
+		{
+			_input1 = input1;
+			_input2 = input2;
+			_expectedResult = expectedResult;
+		}
+
+		void ThenTheResultIsCorrect()
+		{
+            Assert.AreEqual(_input1 + _input2, _expectedResult);
+		}
+	}
+	
+    [WithStory(typeof(CalculatorCanDoFourMainMathFunctions))]
+    public class WhenTwoNumbersAreDevided
+    {
+        void ThenTheResultIsCorrect()
+        {
+            Assert.Inconclusive();
+        }
+    }
+
+    [RunScenarioWithArgs(1, 2, 2)]
+    [RunScenarioWithArgs(4, 5, 20)]
+    [WithStory(typeof(CalculatorCanDoFourMainMathFunctions))]
+    public class WhenTwoNumbersAreMultiplied
+    {
+        private int _expectedResult;
+        private int _input1;
+        private int _input2;
+
+        void RunScenarioWithArgs(int input1, int input2, int expectedResult)
+        {
+            _input1 = input1;
+            _input2 = input2;
+            _expectedResult = expectedResult;
+        }
+
+        void ThenTheResultIsCorrect()
+        {
+            Assert.AreEqual(_input1 * _input2, _expectedResult);
+        }
+    }
+
+    [WithStory(typeof(CalculatorCanDoFourMainMathFunctions))]
+    public class WhenTwoNumbersAreSubtracted
+    {
+        [RunStepWithArgs(5, 3, 2)]
+        [RunStepWithArgs(1, 8, -7)]
+        void ThenTheResultShouldBeCorrect(int input1, int input2, int expectedResult)
+        {
+            Assert.AreEqual(input1 - input2, expectedResult);
         }
     }
 }
