01121836e9efd8924698af064a3a537f3dac1c1d
Mehdi Khalili <me@mehdi-khalili.com>
2011-04-26 11:16:18 +1000

    fixed up some namespaces and assembly infos

--- Bddify.Tests.XUnit/Properties/AssemblyInfo.cs
+++ Bddify.Tests.XUnit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bddify.Tests")]
+[assembly: AssemblyTitle("Bddify.Tests.XUnit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Bddify.Tests")]
+[assembly: AssemblyProduct("Bddify.Tests.XUnit")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
