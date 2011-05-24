f7bfdd43e2a44e19d553f61431c843aca0f019ad
Mehdi Khalili <me@mehdi-khalili.com>
2011-05-24 15:46:48 +1000

    Fixed a few bugs in ExpressionExtensions particularly on NewArrayExpression

--- Bddify/Scanners/ExpressionExtensions.cs
+++ Bddify/Scanners/ExpressionExtensions.cs
@@ -46,15 +46,19 @@ namespace Bddify.Scanners
 
             var constantExpression = expression as ConstantExpression;
             if (constantExpression != null)
-                return ExtractConstants(constantExpression);
+                return ExtractConstants(constantExpression).ToArray();
 
             var newArrayExpression = expression as NewArrayExpression;
             if (newArrayExpression != null)
-                return ExtractConstants(newArrayExpression);
+                return ExtractConstants(newArrayExpression).ToArray();
+
+            var newExpression = expression as NewExpression;
+            if (newExpression != null)
+                return ExtractConstants(newExpression).ToArray();
 
             var unaryExpression = expression as UnaryExpression;
             if (unaryExpression != null)
-                return ExtractConstants(unaryExpression);
+                return ExtractConstants(unaryExpression).ToArray();
 
             throw new InvalidOperationException("Could not fetch the arguments from the provided exception. This may be a bug so please report it");
         }
@@ -64,10 +68,54 @@ namespace Bddify.Scanners
             return ExtractConstants(unaryExpression.Operand);
         }
 
+        private static IEnumerable<object> ExtractConstants(NewExpression newExpression)
+        {
+            throw new NotImplementedException();
+            //var arguments = new List<object>();
+            //foreach (var argumentExpression in newExpression.Arguments)
+            //{
+            //    arguments.Add(ExtractConstants(argumentExpression));
+            //}
+
+            //yield return newExpression.Constructor.Invoke(arguments.ToArray());
+        }
+
         private static IEnumerable<object> ExtractConstants(NewArrayExpression newArrayExpression)
         {
-            var arrayElements = new ArrayList();
             Type type = newArrayExpression.Type.GetElementType();
+            if (type is IConvertible)
+                return ExtractConvertibleTypeArrayConstants(newArrayExpression, type);
+            
+            return ExtractNonConvertibleArrayConstants(newArrayExpression, type);
+        }
+
+        private static IEnumerable<object> ExtractNonConvertibleArrayConstants(NewArrayExpression newArrayExpression, Type type)
+        {
+            dynamic arrayElements = Activator.CreateInstance(typeof(List<>).MakeGenericType(type));
+            foreach (var arrayElementExpression in newArrayExpression.Expressions)
+            {
+                dynamic arrayElement;
+
+                if (arrayElementExpression is ConstantExpression)
+                    arrayElement = ((ConstantExpression)arrayElementExpression).Value;
+                else
+                    arrayElement = ExtractConstants(arrayElementExpression);
+
+                if (arrayElement is object[])
+                {
+                    foreach (dynamic item in (object[])arrayElement)
+                        arrayElements.Add(item);
+                }
+                else
+                    arrayElements.Add(arrayElement);
+            }
+
+            yield return arrayElements.ToArray();
+        }
+
+        private static IEnumerable<object> ExtractConvertibleTypeArrayConstants(NewArrayExpression newArrayExpression, Type type)
+        {
+            var arrayElements = new ArrayList();
             foreach (var arrayElementExpression in newArrayExpression.Expressions)
             {
                 var arrayElement = ((ConstantExpression)arrayElementExpression).Value;
@@ -75,7 +123,6 @@ namespace Bddify.Scanners
             }
 
             yield return arrayElements.ToArray(type);
-
         }
 
         private static IEnumerable<object> ExtractConstants(ConstantExpression constantExpression)
