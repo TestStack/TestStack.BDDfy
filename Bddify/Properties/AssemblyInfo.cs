4d1535880e104c015b8b5ccd5a7759063f81470a
Mehdi Khalili <me@mehdi-khalili.com>
2011-07-01 08:17:15 +1000

    updated the version in nuspec files

--- Bddify/Properties/AssemblyInfo.cs
+++ Bddify/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.5")]
-[assembly: AssemblyFileVersion("0.5")]
+[assembly: AssemblyVersion("0.6")]
+[assembly: AssemblyFileVersion("0.6")]
