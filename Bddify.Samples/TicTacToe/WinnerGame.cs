dd79a8a5b82d6d6f1de4154f01eba6855b60f124
Mehdi Khalili <me@mehdi-khalili.com>
2011-07-01 17:26:38 +1000

    added some comments to the WinnerGame methods

--- Bddify.Samples/TicTacToe/WinnerGame.cs
+++ Bddify.Samples/TicTacToe/WinnerGame.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 
@@ -19,6 +18,9 @@ namespace Bddify.Samples.TicTacToe
             _expectedWinner = expectedWinner;
         }
 
+        // Note: This method returns IEnumerable<string>
+        // this is done to allow the method to return its title.
+        // if you use this method make sure to yield return the title as the very first action
         IEnumerable<string> GivenTheFollowingBoard()
         {
             yield return string.Format(
@@ -30,7 +32,10 @@ namespace Bddify.Samples.TicTacToe
             Game = new Game(_firstRow, _secondRow, _thirdRow);
         }
 
-        IEnumerable<string > ThenTheWinnerShouldBe()
+        // Note: This method returns IEnumerable<string>
+        // this is done to allow the method to return its title.
+        // if you use this method make sure to yield return the title as the very first action
+        IEnumerable<string> ThenTheWinnerShouldBe()
         {
             yield return "Then the winner is " + _expectedWinner;
             Assert.AreEqual(Game.Winner, _expectedWinner);
