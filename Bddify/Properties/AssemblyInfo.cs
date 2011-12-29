6d61a68b0f7eb07cd4e892aeda14c36c2b5536f7
Mehdi Khalili <me@mehdi-khalili.com>
2011-12-29 23:34:09 +1000

    Initiating V1.11

--- Bddify/Properties/AssemblyInfo.cs
+++ Bddify/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.9.1")]
-[assembly: AssemblyFileVersion("0.9.1")]
+[assembly: AssemblyVersion("1.11")]
+[assembly: AssemblyFileVersion("1.11")]
