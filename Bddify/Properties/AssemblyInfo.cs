6824ef7a85bc36d6362586650b201c722bb5574e
Mehdi Khalili <me@mehdi-khalili.com>
2012-01-10 20:23:17 +1000

    Releasing V1.15

--- Bddify/Properties/AssemblyInfo.cs
+++ Bddify/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.14")]
-[assembly: AssemblyFileVersion("1.14")]
+[assembly: AssemblyVersion("1.15")]
+[assembly: AssemblyFileVersion("1.15")]
