1822bf3885983e07ca2d8227e47c05e5602e2b48
Mehdi Khalili <me@mehdi-khalili.com>
2012-05-03 21:29:01 +1000

    Released V2.10

--- Bddify/Properties/AssemblyInfo.cs
+++ Bddify/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.0")]
+[assembly: AssemblyVersion("2.10")]
+[assembly: AssemblyFileVersion("2.10")]
