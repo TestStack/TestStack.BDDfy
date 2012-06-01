4281e580357b20bac49933822aa4a0233e1fcfd7
Michael Whelan <mjmwdev@gmail.com>
2012-06-01 07:42:19 +0100

    HTML report filtering by step execution result.
    Modified bddify.js and bddify.cs to provide filtering of HTML report by
    step execution result.

--- BDDfy/Processors/HtmlReporter/BDDfy.js
+++ BDDfy/Processors/HtmlReporter/BDDfy.js
@@ -13,4 +13,15 @@
     $('.collapseAll').click(function () {
         $('.steps').css('display', 'none');
     });
+
+    $("ul.resultSummary li:not('.storySummary'):not('.scenarioSummary')")
+        .append("<input type='checkbox' class='cbx_toggle' checked/>");
+
+    $(".cbx_toggle").click(function () {
+
+        var checkBox = $(this);
+        var resultType = checkBox.closest("li").attr("class");
+
+        $("#testResult div.scenario ." + resultType).parent().toggle(checkBox.is(":checked"));
+    });
 });
