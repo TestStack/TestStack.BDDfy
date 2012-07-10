747ee87e2a0ed2017f1887e494626558d0b6d7d5
MehdiK <me@mehdi-khalili.com>
2012-07-10 21:08:07 +1000

    Assembly and nuget versions updated to V3.11

--- TestStack.BDDfy/Properties/AssemblyInfo.cs
+++ TestStack.BDDfy/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.10")]
-[assembly: AssemblyFileVersion("3.10")]
+[assembly: AssemblyVersion("3.11")]
+[assembly: AssemblyFileVersion("3.11")]
