4057a22c1ccd5b71384a84ef13ab357db216d825
MehdiK <me@mehdi-khalili.com>
2012-08-17 12:48:11 +1000

    Created redirection for the old bddify packages to new ones

--- NuGet/BddifyTicTacToe-upgrade-notice/tools/install.ps1
+++ NuGet/BddifyTicTacToe-upgrade-notice/tools/install.ps1
@@ -1,4 +1,4 @@
 param($installPath, $toolsPath, $package, $project) 
 $path = [System.IO.Path] 
-$readmefile = $path::Combine($installPath, "content\bddify.readme.txt") 
+$readmefile = $path::Combine($installPath, "content\bddify.tictactoe.readme.txt") 
 $DTE.ItemOperations.OpenFile($readmefile) 
---
+++ go.oldpackages.cmd
@@ -0,0 +1,3 @@
+.\Resource\Tool\NuGet\NuGet.exe pack .\NuGet\bddify-upgrade-notice\Bddify.nuspec
+.\Resource\Tool\NuGet\NuGet.exe pack .\NuGet\bddifyAtm-upgrade-notice\Bddify.Samples.Atm.nuspec
+.\Resource\Tool\NuGet\NuGet.exe pack .\NuGet\bddifyTicTacToe-upgrade-notice\Bddify.Samples.TicTacToe.nuspec
