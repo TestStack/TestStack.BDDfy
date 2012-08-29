b2a3d1af874d2b62c8f759a8bf4c1eb4623f580f
Dmytro Zharii <Dmytro_Zharii@epam.com>
2012-08-29 15:09:47 +0300

    I was not able to use BDDfy in the abstract class because of existing of new() constraint in Given/When/Then fluent extensions. Not the constraint is removed from FluentScannerExtensions.cs

--- TestStack.BDDfy/Scanners/StepScanners/Fluent/FluentScannerExtensions.cs
+++ TestStack.BDDfy/Scanners/StepScanners/Fluent/FluentScannerExtensions.cs
@@ -33,43 +33,43 @@ namespace TestStack.BDDfy.Scanners.StepScanners.Fluent
 {
     public static class FluentStepScannerExtensions
     {
-        static IInitialStep<TScenario> Scan<TScenario>(this TScenario testObject) where TScenario : class, new()
+        static IInitialStep<TScenario> Scan<TScenario>(this TScenario testObject) where TScenario : class
         {
             return new FluentScanner<TScenario>(testObject);
         }
 
         public static IGiven<TScenario> Given<TScenario>(this TScenario testObject, Expression<Action<TScenario>> givenStep, string stepTextTemplate)
-            where TScenario: class, new()
+            where TScenario: class
         {
             return testObject.Scan().Given(givenStep, stepTextTemplate);
         }
 
         public static IGiven<TScenario> Given<TScenario>(this TScenario testObject, Expression<Action<TScenario>> givenStep, bool includeInputsInStepTitle)
-            where TScenario: class, new()
+            where TScenario: class
         {
             return testObject.Scan().Given(givenStep, includeInputsInStepTitle);
         }
 
         public static IWhen<TScenario> When<TScenario>(this TScenario testObject, Expression<Action<TScenario>> whenStep, string stepTextTemplate)
-            where TScenario : class, new()
+            where TScenario : class
         {
             return testObject.Scan().When(whenStep, stepTextTemplate);
         }
 
         public static IWhen<TScenario> When<TScenario>(this TScenario testObject, Expression<Action<TScenario>> whenStep, bool includeInputsInStepTitle)
-            where TScenario : class, new()
+            where TScenario : class
         {
             return testObject.Scan().When(whenStep, includeInputsInStepTitle);
         }
 
         public static IGiven<TScenario> Given<TScenario>(this TScenario testObject, Expression<Action<TScenario>> givenStep)
-            where TScenario: class, new()
+            where TScenario: class
         {
             return testObject.Given(givenStep, null);
         }
 
         public static IWhen<TScenario> When<TScenario>(this TScenario testObject, Expression<Action<TScenario>> whenStep)
-            where TScenario : class, new()
+            where TScenario : class
         {
             return testObject.When(whenStep, null);
         }
