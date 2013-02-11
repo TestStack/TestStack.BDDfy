b3feed1ef82c3427aa39fee554e1874e68c7cef2
Michael Whelan <mjmwdev@gmail.com>
2013-02-11 08:04:20 +0000

    Fixed json parsing

--- TestStack.BDDfy/Processors/Diagnostics/JsonReportBuilder.cs
+++ TestStack.BDDfy/Processors/Diagnostics/JsonReportBuilder.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 
 namespace TestStack.BDDfy.Processors.Diagnostics
@@ -30,33 +29,35 @@ namespace TestStack.BDDfy.Processors.Diagnostics
                         AddJsonLine("StoryName", story.MetaData.Title);
                         AddJsonLine("StoryDuration", story.Scenarios.Sum(x => x.Duration.Milliseconds).ToString());
                         AddLine("\"Scenarios\":");
-                        AddLine("[");
-
-                        int scenarioCount = 0;
-                        foreach (var scenario in story.Scenarios)
+                        using(OpenTag("[", "]"))
                         {
-                            scenarioCount++;
-                            using (OpenTag("{", "}"))
+                            int scenarioCount = 0;
+                            foreach (var scenario in story.Scenarios)
                             {
-                                AddJsonLine("ScenarioName", scenario.Title);
-                                AddJsonLine("ScenarioDuration", scenario.Duration.Milliseconds.ToString());
-                                AddLine("\"Steps\":");
-
-                                int stepCount = 0;
-                                foreach (var step in scenario.Steps)
+                                scenarioCount++;
+                                using (OpenTag("{", "}"))
                                 {
-                                    stepCount++;
-                                    using (OpenTag("{", "}"))
+                                    AddJsonLine("ScenarioName", scenario.Title);
+                                    AddJsonLine("ScenarioDuration", scenario.Duration.Milliseconds.ToString());
+                                    AddLine("\"Steps\":");
+                                    using (OpenTag("[", "]"))
                                     {
-                                        AddJsonLine("StepName", step.StepTitle);
-                                        AddJsonLine("StepDuration", step.Duration.Milliseconds.ToString(), false);                                        
+                                        int stepCount = 0;
+                                        foreach (var step in scenario.Steps)
+                                        {
+                                            stepCount++;
+                                            using (OpenTag("{", "}"))
+                                            {
+                                                AddJsonLine("StepName", step.StepTitle);
+                                                AddJsonLine("StepDuration", step.Duration.Milliseconds.ToString(), false);
+                                            }
+                                            if (stepCount < scenario.Steps.Count) _document.Append(",");
+                                        }
                                     }
-                                    if (stepCount < scenario.Steps.Count) _document.Append(",");
                                 }
+                                if (scenarioCount < story.Scenarios.Count()) _document.Append(",");
                             }
-                            if(scenarioCount < story.Scenarios.Count()) _document.Append(",");
                         }
-                        AddLine("]");
                     }
                     if(storyCount < _viewModel.Stories.Count()) _document.Append(",");
                 }
@@ -86,10 +87,58 @@ namespace TestStack.BDDfy.Processors.Diagnostics
 
         private void AddJsonLine(string propertyName, string value, bool appendComma = true)
         {
-            var line = string.Format("\"{0}\":\"{1}\"", propertyName, value);
+            var line = string.Format("\"{0}\":{1}", propertyName, EncodeJsString(value));
             if (appendComma)
                 line += ",";
             AddLine(line);
+        }
+
+        // http://www.west-wind.com/weblog/posts/2007/Jul/14/Embedding-JavaScript-Strings-from-an-ASPNET-Page
+        private string EncodeJsString(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\"");
+            foreach (char c in s)
+            {
+                switch (c)
+                {
+                    case '\"':
+                        sb.Append("\\\"");
+                        break;
+                    case '\\':
+                        sb.Append("\\\\");
+                        break;
+                    case '\b':
+                        sb.Append("\\b");
+                        break;
+                    case '\f':
+                        sb.Append("\\f");
+                        break;
+                    case '\n':
+                        sb.Append("\\n");
+                        break;
+                    case '\r':
+                        sb.Append("\\r");
+                        break;
+                    case '\t':
+                        sb.Append("\\t");
+                        break;
+                    default:
+                        int i = (int)c;
+                        if (i < 32 || i > 127)
+                        {
+                            sb.AppendFormat("\\u{0:X04}", i);
+                        }
+                        else
+                        {
+                            sb.Append(c);
+                        }
+                        break;
+                }
+            }
+            sb.Append("\"");
+
+            return sb.ToString();
         }
     }
 }
