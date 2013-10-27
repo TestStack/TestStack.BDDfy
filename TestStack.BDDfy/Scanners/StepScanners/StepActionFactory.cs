7cc3b19421c624143aceb005b03a1b7412d470d5
Jake Ginnivan <jake.ginnivan@readify.net>
2013-10-27 15:28:33 +0000

    Pulled out a common run method

--- TestStack.BDDfy/Scanners/StepScanners/StepActionFactory.cs
+++ TestStack.BDDfy/Scanners/StepScanners/StepActionFactory.cs
@@ -7,13 +7,18 @@ namespace TestStack.BDDfy.Scanners.StepScanners
     {
         public static Action<object> GetStepAction(MethodInfo method, object[] inputs)
         {
-            return o => method.Invoke(o, inputs);
+            return o => Run(()=>method.Invoke(o, inputs));
         }
 
         public static Action<object> GetStepAction<TScenario>(Action<TScenario> action)
             where TScenario : class
         {
-            return o => action((TScenario) o);
+            return o => Run(() => action((TScenario)o));
+        }
+
+        private static void Run(Action func)
+        {
+            func();
         }
     }
 }
