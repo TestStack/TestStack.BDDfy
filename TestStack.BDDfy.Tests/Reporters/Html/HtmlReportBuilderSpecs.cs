06c362913f77dc3a8ad89d43f95853ffeca8c0e9
Michael Whelan <mjmwdev@gmail.com>
2014-03-24 18:16:35 +0000

    Added HtmlReportBuilder test.

---
+++ TestStack.BDDfy.Tests/Reporters/Html/HtmlReportBuilderSpecs.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using TestStack.BDDfy.Reporters.Html;
+
+namespace TestStack.BDDfy.Tests.Reporters.Html
+{
+    [TestFixture]
+    public class HtmlReportBuilderSpecs
+    {
+        [Test]
+        public void ShouldProduceExpectedHtml()
+        {
+            string expected = GetReportHtml();
+            var model = new HtmlReportViewModel(
+                new DefaultHtmlReportConfiguration(), 
+                new ReportTestData().CreateTwoStoriesEachWithTwoScenariosWithThreeStepsOfFiveMilliseconds());
+            var sut = new HtmlReportBuilder();
+
+            var result = sut.CreateReport(model);
+
+            // would prefer to assert the string contents but different IDs are generated each time so reports are never exacty the same.
+            Assert.That(result.Length, Is.EqualTo(expected.Length));
+        }
+
+        private string GetReportHtml()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "TestStack.BDDfy.Tests.Reporters.Html.HtmlReport.html";
+
+            string result;
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            return result;
+        }
+    }
+}
