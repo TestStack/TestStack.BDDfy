509f876cacb2f016deff9f0f1933d4ec56666e06
MehdiK <me@mehdi-khalili.com>
2014-03-29 18:38:28 +0430

    trying to get the approval test working

--- TestStack.BDDfy.Tests/Reporters/Html/HtmlReportBuilderTests.cs
+++ TestStack.BDDfy.Tests/Reporters/Html/HtmlReportBuilderTests.cs
@@ -10,42 +10,21 @@ using TestStack.BDDfy.Reporters.Html;
 namespace TestStack.BDDfy.Tests.Reporters.Html
 {
     [TestFixture]
-    [UseReporter(typeof(DiffReporter))]
+    [UseReporter(typeof (DiffReporter))]
     public class HtmlReportBuilderTests
     {
         [Test]
         [MethodImpl(MethodImplOptions.NoInlining)]
         public void ShouldProduceExpectedHtml()
         {
-            string expected = GetReportHtml(); 
             var model = new HtmlReportViewModel(
-                new DefaultHtmlReportConfiguration(), 
+                new DefaultHtmlReportConfiguration(),
                 new ReportTestData().CreateTwoStoriesEachWithTwoScenariosWithThreeStepsOfFiveMilliseconds());
-            var sut = new HtmlReportBuilder();
-            sut.DateProvider = () => new DateTime(2014, 3, 25, 11, 30,5);
+            var sut = new HtmlReportBuilder {DateProvider = () => new DateTime(2014, 3, 25, 11, 30, 5)};
 
             var result = sut.CreateReport(model);
 
-            //Approvals.Verify(result);
-            Assert.That(result, Is.EqualTo(expected));
+            Approvals.Verify(result);
         }
-
-        private string GetReportHtml()
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceName = "TestStack.BDDfy.Tests.Reporters.Html.HtmlReportBuilderTests.ShouldProduceExpectedHtml.approved.txt";
-
-            string result;
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    result = reader.ReadToEnd();
-                }
-            }
-
-            return result;
-        }
-
     }
 }
