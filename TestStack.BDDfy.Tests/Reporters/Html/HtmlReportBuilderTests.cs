1b8fb0c56e02374c83ba253359a509676ca8fba2
MehdiK <me@mehdi-khalili.com>
2014-03-30 21:36:09 +0430

    added TemporaryCulture to HtmlReportBuilderTests

--- TestStack.BDDfy.Tests/Reporters/Html/HtmlReportBuilderTests.cs
+++ TestStack.BDDfy.Tests/Reporters/Html/HtmlReportBuilderTests.cs
@@ -25,17 +25,32 @@ namespace TestStack.BDDfy.Tests.Reporters.Html
             // resetting here explicitly
             Configurator.IdGenerator.Reset();
 
-            var model = new HtmlReportViewModel(
-                new DefaultHtmlReportConfiguration(),
-                new ReportTestData().CreateTwoStoriesEachWithTwoScenariosWithThreeStepsOfFiveMilliseconds());
-
-            var sut = new HtmlReportBuilder {DateProvider = () => new DateTime(2014, 3, 25, 11, 30, 5)};
-
             // setting the culture to make sure the date is formatted the same on all machines
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-GB");
-            
-            var result = sut.CreateReport(model);
-            Approvals.Verify(result);
+            using (new TemporaryCulture("en-GB"))
+            {
+                var model = new HtmlReportViewModel(
+                    new DefaultHtmlReportConfiguration(),
+                    new ReportTestData().CreateTwoStoriesEachWithTwoScenariosWithThreeStepsOfFiveMilliseconds());
+
+                var sut = new HtmlReportBuilder {DateProvider = () => new DateTime(2014, 3, 25, 11, 30, 5)};
+                var result = sut.CreateReport(model);
+                Approvals.Verify(result);
+            }
+        }
+    }
+
+    public class TemporaryCulture : IDisposable
+    {
+        private readonly string _originalCulture;
+        public TemporaryCulture(string newCulture)
+        {
+            _originalCulture = Thread.CurrentThread.CurrentCulture.Name;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(newCulture);
+        }
+
+        public void Dispose()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(_originalCulture);
         }
     }
 }
