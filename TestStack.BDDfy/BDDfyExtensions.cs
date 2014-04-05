409a32e60081a769f429da84820a3086e7e04a04
MehdiK <me@mehdi-khalili.com>
2014-04-05 11:46:27 +0430

    removed now unnecessary BDDfy extensions

--- TestStack.BDDfy/BDDfyExtensions.cs
+++ TestStack.BDDfy/BDDfyExtensions.cs
@@ -18,36 +18,10 @@ namespace TestStack.BDDfy
         /// Extension method to BDDfy an object instance.
         /// </summary>
         /// <param name="testObject">The test object representing a scenario.</param>
-        /// <returns></returns>
-        public static Story BDDfy(this object testObject)
-        {
-            return BDDfy(testObject, null);
-        }
-
-        public static Story BDDfy(this IExamples examples)
-        {
-            return examples.TestObject.BDDfy();
-        }
-
-        /// <summary>
-        /// Extension method to BDDfy an object instance.
-        /// </summary>
-        /// <param name="testObject">The test object representing a scenario.</param>
-        /// <param name="scenarioTitle">Overrides the default scenario title and is displayed in the reports.</param>
-        /// <returns></returns>
-        public static Story BDDfy(this object testObject, string scenarioTitle)
-        {
-            return testObject.LazyBDDfy(scenarioTitle).Run();
-        }
-
-        /// <summary>
-        /// Extension method to BDDfy an object instance.
-        /// </summary>
-        /// <param name="testObject">The test object representing a scenario.</param>
         /// <param name="scenarioTitle">Overrides the default scenario title and is displayed in the reports.</param>
         /// <param name="storyCategory">Used for filename in Html reports. Has no effect on console reports.</param>
         /// <returns></returns>
-        public static Story BDDfy(this object testObject, string scenarioTitle, string storyCategory)
+        public static Story BDDfy(this object testObject, string scenarioTitle = null, string storyCategory = null)
         {
             return testObject.LazyBDDfy(scenarioTitle, storyCategory).Run();
         }
@@ -62,35 +36,10 @@ namespace TestStack.BDDfy
         /// </summary>
         /// <typeparam name="TStory">The type representing the story.</typeparam>
         /// <param name="testObject">The test object representing a scenario.</param>
-        /// <returns></returns>
-        public static Story BDDfy<TStory>(this object testObject)
-            where TStory : class
-        {
-            return BDDfy<TStory>(testObject, null);
-        }
-
-        /// <summary>
-        /// Extension method to BDDfy an object instance.
-        /// </summary>
-        /// <typeparam name="TStory">The type representing the story.</typeparam>
-        /// <param name="testObject">The test object representing a scenario.</param>
-        /// <param name="scenarioTitle">Overrides the default scenario title and is displayed in the reports.</param>
-        /// <returns></returns>
-        public static Story BDDfy<TStory>(this object testObject, string scenarioTitle)
-            where TStory : class
-        {
-            return testObject.LazyBDDfy<TStory>(scenarioTitle).Run();
-        }
-
-        /// <summary>
-        /// Extension method to BDDfy an object instance.
-        /// </summary>
-        /// <typeparam name="TStory">The type representing the story.</typeparam>
-        /// <param name="testObject">The test object representing a scenario.</param>
         /// <param name="scenarioTitle">Overrides the default scenario title and is displayed in the reports.</param>
         /// <param name="storyCategory">Used for filename in Html reports. Has no effect on console reports.</param>
         /// <returns></returns>
-        public static Story BDDfy<TStory>(this object testObject, string scenarioTitle, string storyCategory)
+        public static Story BDDfy<TStory>(this object testObject, string scenarioTitle = null, string storyCategory = null)
             where TStory : class
         {
             return testObject.LazyBDDfy<TStory>(scenarioTitle, storyCategory).Run();
