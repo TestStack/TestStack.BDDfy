239bc6047a3f049a22acec82929bde8d21c3c56e
Jason Roberts <osjasonroberts@gmail.com>
2014-04-07 14:06:41 +0800

    added extra test to check that the metro builder actually works when configured in a test

---
+++ TestStack.BDDfy.Tests/Reporters/HtmlMetro/ConfiguredHtmlMetroReportTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using TestStack.BDDfy.Configuration;
+using TestStack.BDDfy.Reporters.Html;
+using TestStack.BDDfy.Reporters.HtmlMetro;
+using TestStack.BDDfy.Reporters.Readers;
+using TestStack.BDDfy.Reporters.Writers;
+
+namespace TestStack.BDDfy.Tests.Reporters.HtmlMetro
+{
+    /// <summary>
+    /// Wasn't sure where/if to put this test
+    /// </summary>
+    [TestFixture]
+    public class ConfiguredHtmlMetroReportTests
+    {
+        [Test]
+        public void ConfiguringTheHtmlMetroReporterRunsWithoutError()
+        {
+            Configurator.BatchProcessors.Add(new HtmlReporter(new DefaultHtmlReportConfiguration(), new HtmlMetroReportBuilder(), new FileWriter(),
+                new FileReader()));
+
+            Configurator.BatchProcessors.HtmlReport.Disable();
+
+            this.Given(x => GivenA())
+                    .When(x => WhenB())
+                    .Then(x => ThenC())
+                .BDDfy();
+        }
+
+
+        private void GivenA()
+        {
+        }
+
+        private void WhenB()
+        {
+        }
+        
+        private void ThenC()
+        {
+        }
+    }
+}
