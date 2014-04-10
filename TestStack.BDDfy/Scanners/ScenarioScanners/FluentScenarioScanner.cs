c158b505deff6393481c05bab71e34c4115e3912
Jake Ginnivan <jake.ginnivan@readify.net>
2014-04-10 09:56:29 +0100

    Removed unused namespaces

--- TestStack.BDDfy/Scanners/ScenarioScanners/FluentScenarioScanner.cs
+++ TestStack.BDDfy/Scanners/ScenarioScanners/FluentScenarioScanner.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
-using System.Reflection;
 using TestStack.BDDfy.Configuration;
 
 namespace TestStack.BDDfy
