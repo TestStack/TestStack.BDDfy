00ac022bf6ca72055effcb33ba4cfdecadf58bdb
Jake Ginnivan <jake.ginnivan@readify.net>
2014-04-15 07:50:23 +0100

    Made it work with a few more types

--- TestStack.BDDfy/Scanners/StepScanners/Examples/ExampleValue.cs
+++ TestStack.BDDfy/Scanners/StepScanners/Examples/ExampleValue.cs
@@ -16,15 +16,26 @@ namespace TestStack.BDDfy
 
         public object GetExampleValue(Type targetType)
         {
-            if (_underlyingValue == null || (_underlyingValue is string && string.IsNullOrEmpty(_underlyingValue as string)))
+            var stringValue = _underlyingValue as string;
+            if (_underlyingValue == null || (_underlyingValue is string && string.IsNullOrEmpty(stringValue)))
             {
-                if (targetType.IsValueType && !(targetType.IsGenericType && targetType.GetGenericTypeDefinition() == typeof(Nullable<>)))
-                    throw new ArgumentException("Cannot convert TODO");
+                if (targetType.IsValueType &&
+                    !(targetType.IsGenericType && targetType.GetGenericTypeDefinition() == typeof (Nullable<>)))
+                {
+                    var valueAsString = string.IsNullOrEmpty(stringValue) ? "<null>" : string.Format("\"{0}\"", _underlyingValue);
+                    throw new ArgumentException(string.Format("Cannot convert {0} to {1}", valueAsString, targetType.Name));
+                }
                 return null;
             }
 
             if (targetType.IsInstanceOfType(_underlyingValue))
                 return _underlyingValue;
+
+            if (targetType.IsEnum && _underlyingValue is string)
+                return Enum.Parse(targetType, (string)_underlyingValue);
+
+            if (targetType == typeof (DateTime))
+                return DateTime.Parse(stringValue);
 
             return Convert.ChangeType(_underlyingValue, targetType);
         }
--- TestStack.BDDfy/Scanners/StepScanners/Examples/WithExamplesExtensions.cs
+++ TestStack.BDDfy/Scanners/StepScanners/Examples/WithExamplesExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace TestStack.BDDfy
+﻿using System.Linq;
+
+namespace TestStack.BDDfy
 {
     public static class WithExamplesExtensions
     {
@@ -6,6 +8,31 @@
         {
             table.TestObject = testObject;
             return table;
+        }
+
+        public static ExampleTable WithExamples(this object testObject, string table)
+        {
+            return testObject.WithExamples(Parse(table));
+        }
+
+        static ExampleTable Parse(string table)
+        {
+            var lines = table.Trim().Split('\n').Select(l => l.Trim().Trim('|'));
+            var firstRow = lines.First();
+            var headers = firstRow.Split('|').Select(h => h.Trim().Replace(" ", string.Empty)).ToArray();
+
+            var exampleTable = new ExampleTable(headers);
+            foreach (var line in lines.Skip(1))
+            {
+                exampleTable.Add(GetValues(line));
+            }
+
+            return exampleTable;
+        }
+
+        static object[] GetValues(string row)
+        {
+            return row.Split('|').Select(h => (object)h.Trim()).ToArray();
         }
     }
 }
