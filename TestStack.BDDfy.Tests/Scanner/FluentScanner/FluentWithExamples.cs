c1aadf421006ca2f2014be795b9d18c1d76eddaf
Jake Ginnivan <jake.ginnivan@readify.net>
2014-04-16 20:36:55 +0100

    Support private fields

--- TestStack.BDDfy.Tests/Scanner/FluentScanner/FluentWithExamples.cs
+++ TestStack.BDDfy.Tests/Scanner/FluentScanner/FluentWithExamples.cs
@@ -15,18 +15,24 @@ namespace TestStack.BDDfy.Tests.Scanner.FluentScanner
                 .Given(_ => GivenMethodTaking__ExampleInt__(Prop1), false)
                 .And(_ => GivenMethodTaking__ExampleInt__(_.Prop1), false)
                 .And(_ => GivenADifferentMethodWithRandomArg(2))
+                .And(_ => GivenADifferentMethodWith(_prop2))
                 .When(_ => WhenMethodUsing__ExampleString__())
                 .Then(_ => ThenAllIsGood())
-                .WithExamples(new ExampleTable("Prop 1", "Prop2")
+                .WithExamples(new ExampleTable("Prop 1", "Prop2", "Prop 3")
                 {
-                    {1, "foo"},
-                    {2, "bar"}
+                    {1, "foo", ExecutionOrder.ConsecutiveAssertion },
+                    {2, "bar", ExecutionOrder.Initialize }
                 })
                 .BDDfy();
 
             var textReporter = new TextReporter();
             textReporter.Process(story);
             Approvals.Verify(textReporter.ToString());
+        }
+
+        private void GivenADifferentMethodWith(string prop2)
+        {
+            _prop2.ShouldBeOneOf("foo", "bar");
         }
 
         private void GivenADifferentMethodWithRandomArg(int foo)
@@ -41,7 +47,8 @@ namespace TestStack.BDDfy.Tests.Scanner.FluentScanner
 
         private void WhenMethodUsing__ExampleString__()
         {
-            Prop2.ShouldBeOneOf("foo", "bar");
+            _prop2.ShouldBeOneOf("foo", "bar");
+            Prop_3.ShouldBeOneOf(ExecutionOrder.ConsecutiveAssertion, ExecutionOrder.Initialize);
         }
 
         private void GivenMethodTaking__ExampleInt__(int exampleInt)
@@ -50,6 +57,7 @@ namespace TestStack.BDDfy.Tests.Scanner.FluentScanner
         }
 
         public int Prop1 { get; set; }
-        public string Prop2 { get; set; }
+        private string _prop2;
+        public ExecutionOrder Prop_3 { get; set; }
     }
 }
