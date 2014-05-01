9bf03bbca8d14c229377d28430602b3231159257
Jake Ginnivan <jake.ginnivan@readify.net>
2014-05-01 16:28:35 +0100

    Updated markdown report for examples

--- TestStack.BDDfy.Tests/Reporters/MarkDown/MarkDownReportBuilderTests.cs
+++ TestStack.BDDfy.Tests/Reporters/MarkDown/MarkDownReportBuilderTests.cs
@@ -17,5 +17,19 @@ namespace TestStack.BDDfy.Tests.Reporters.MarkDown
             var sut = new MarkDownReportBuilder();
             ReportApprover.Approve(model, sut);
         }
+
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void ShouldProduceExpectedMarkdownWithExamples()
+        {
+            Func<FileReportModel> model = () =>
+                new FileReportModel(new ReportTestData().CreateTwoStoriesEachWithOneFailingScenarioAndOnePassingScenarioWithThreeStepsOfFiveMillisecondsAndEachHasTwoExamples())
+                {
+                    RunDate = new DateTime(2014, 3, 25, 11, 30, 5)
+                };
+
+            var sut = new MarkDownReportBuilder();
+            ReportApprover.Approve(model, sut);
+        }
     }
 }
--- TestStack.BDDfy/Reporters/MarkDown/MarkDownReportBuilder.cs
+++ TestStack.BDDfy/Reporters/MarkDown/MarkDownReportBuilder.cs
@@ -1,15 +1,21 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace TestStack.BDDfy.Reporters.MarkDown
 {
     public class MarkDownReportBuilder : IReportBuilder
     {
+        private readonly List<Exception> _exceptions = new List<Exception>();
+
         public string CreateReport(FileReportModel model)
         {
             var report = new StringBuilder();
 
             foreach (var story in model.Stories)
             {
+                _exceptions.Clear();
                 if (story.Metadata != null)
                 {
                     report.AppendLine(string.Format("## {0}{1}", story.Metadata.TitlePrefix, story.Metadata.Title));
@@ -22,19 +28,154 @@ namespace TestStack.BDDfy.Reporters.MarkDown
                 }
 
                 report.AppendLine(); // separator
-            
-                foreach (var scenario in story.Scenarios)
+
+                foreach (var scenarioGroup in story.Scenarios.GroupBy(s => s.Id))
                 {
-                    report.AppendLine(string.Format("### {0}", scenario.Title));
+                    if (scenarioGroup.Count() > 1)
+                    {
+                        // all scenarios in an example based scenario share the same header and narrative
+                        var exampleScenario = story.Scenarios.First();
+                        report.AppendLine(string.Format("### {0}", exampleScenario.Title));
 
-                    foreach (var step in scenario.Steps)
-                        report.AppendLine("  " + step.Title + "  ");
+                        if (exampleScenario.Steps.Any())
+                        {
+                            foreach (var step in exampleScenario.Steps.Where(s => s.ShouldReport))
+                                report.AppendLine("  " + step.Title + "  ");
+                        }
 
-                    report.AppendLine(); // separator
+                        report.AppendLine(); // separator
+                        WriteExamples(report, exampleScenario, scenarioGroup);
+                    }
+                    else
+                    {
+                        foreach (var scenario in scenarioGroup)
+                        {
+                            report.AppendLine(string.Format("### {0}", scenario.Title));
+
+                            foreach (var step in scenario.Steps)
+                                report.AppendLine("  " + step.Title + "  ");
+
+                            report.AppendLine(); // separator
+                        }
+                    }
                 }
+
+                ReportExceptions(report);
             }
+            _exceptions.Clear();
 
             return report.ToString();
+        }
+
+        private void WriteExamples(StringBuilder report, Scenario exampleScenario, IEnumerable<Scenario> scenarioGroup)
+        {
+            report.AppendLine("### Examples: ");
+            report.AppendLine();
+            var scenarios = scenarioGroup.ToArray();
+            var allPassed = scenarios.All(s => s.Result == Result.Passed);
+            var exampleColumns = exampleScenario.Example.Headers.Length;
+            var numberColumns = allPassed ? exampleColumns : exampleColumns + 2;
+            var maxWidth = new int[numberColumns];
+            var rows = new List<string[]>();
+
+            Action<IEnumerable<string>, string, string> addRow = (cells, result, error) =>
+            {
+                var row = new string[numberColumns];
+                var index = 0;
+
+                foreach (var cellText in cells)
+                    row[index++] = cellText;
+
+                if (!allPassed)
+                {
+                    row[numberColumns - 2] = result;
+                    row[numberColumns - 1] = error;
+                }
+
+                for (var i = 0; i < numberColumns; i++)
+                {
+                    var rowValue = row[i];
+                    if (rowValue != null && rowValue.Length > maxWidth[i])
+                        maxWidth[i] = rowValue.Length;
+                }
+
+                rows.Add(row);
+            };
+
+            addRow(exampleScenario.Example.Headers, "Result", "Errors");
+            foreach (var scenario in scenarios)
+            {
+                var failingStep = scenario.Steps.FirstOrDefault(s => s.Result == Result.Failed);
+                var error = failingStep == null
+                    ? null
+                    : string.Format("Step: {0} failed with exception: {1}", failingStep.Title, CreateExceptionMessage(failingStep));
+
+                addRow(scenario.Example.Values.Select(e => e.GetValueAsString()), scenario.Result.ToString(), error);
+            }
+
+            foreach (var row in rows)
+                WriteExampleRow(report, row, maxWidth);
+        }
+
+        private void WriteExampleRow(StringBuilder report, string[] row, int[] maxWidth)
+        {
+            report.Append("    ");
+            for (int index = 0; index < row.Length; index++)
+            {
+                var col = row[index];
+                report.AppendFormat("| {0} ", (col ?? string.Empty).Trim().PadRight(maxWidth[index]));
+            }
+            report.AppendLine("|");
+        }
+
+        private string CreateExceptionMessage(Step step)
+        {
+            _exceptions.Add(step.Exception);
+
+            var exceptionReference = string.Format("[Details at {0} below]", _exceptions.Count);
+            if (!string.IsNullOrEmpty(step.Exception.Message))
+                return string.Format("[{0}] {1}", FlattenExceptionMessage(step.Exception.Message), exceptionReference);
+
+            return string.Format("{0}", exceptionReference);
+        }
+
+        void ReportExceptions(StringBuilder report)
+        {
+            if (_exceptions.Count == 0)
+                return;
+
+            report.AppendLine();
+            report.Append("#### Exceptions:");
+
+            for (int index = 0; index < _exceptions.Count; index++)
+            {
+                var exception = _exceptions[index];
+                report.AppendLine();
+                report.AppendFormat("    {0}. ", index + 1);
+
+                if (!string.IsNullOrEmpty(exception.Message))
+                {
+                    report.AppendLine(FlattenExceptionMessage(exception.Message));
+                }
+                else
+                    report.AppendLine();
+
+                var stackTrace = string.Join(Environment.NewLine, exception.StackTrace.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+                    .Select(s => "    " + s));
+                report.AppendLine(stackTrace);
+            }
+
+            report.AppendLine();
+        }
+
+        static string FlattenExceptionMessage(string message)
+        {
+            return message
+                .Replace("\t", " ") // replace tab with one space
+                .Replace(Environment.NewLine, ", ") // replace new line with one space
+                .Trim() // trim starting and trailing spaces
+                .Replace("  ", " ")
+                .TrimEnd(','); // chop any , from the end
         }
     }
 }
