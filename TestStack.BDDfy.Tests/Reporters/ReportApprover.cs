48857ab1c654da3c26e26c32e7dc5743cb99de1b
Jake Ginnivan <jake.ginnivan@readify.net>
2014-05-07 15:17:20 +0100

    Trying to fix failing test

--- TestStack.BDDfy.Tests/Reporters/ReportApprover.cs
+++ TestStack.BDDfy.Tests/Reporters/ReportApprover.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 using ApprovalTests;
-using TestStack.BDDfy.Configuration;
 using TestStack.BDDfy.Reporters;
 using TestStack.BDDfy.Tests.Reporters.Html;
 
@@ -11,10 +10,6 @@ namespace TestStack.BDDfy.Tests.Reporters
     {
         public static void Approve(Func<FileReportModel> model, IReportBuilder reportBuilder)
         {
-            // somehow the scenario id keeps increasing on TC
-            // resetting here explicitly
-            Configurator.IdGenerator.Reset();
-
             // setting the culture to make sure the date is formatted the same on all machines
             using (new TemporaryCulture("en-GB"))
             {
---
+++ TestStack.BDDfy.Tests/Setup.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TestStack.BDDfy.Configuration;
+
+namespace TestStack.BDDfy.Tests
+{
+    [SetUpFixture]
+    public class Setup
+    {
+        [SetUp]
+        public void RunBeforeAnyTests()
+        {
+            // somehow the scenario id keeps increasing on TC
+            // resetting here explicitly
+            Configurator.IdGenerator.Reset();
+        }
+    }
+}
