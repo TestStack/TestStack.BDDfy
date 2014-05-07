fd689804f542b4d2bae4d548ecdbc279846ef781
Jake Ginnivan <jake.ginnivan@readify.net>
2014-05-07 13:59:03 +0100

    Made StepArgument evaluation lazy

--- TestStack.BDDfy.Tests/Scanner/FluentScanner/StepTitleTests.cs
+++ TestStack.BDDfy.Tests/Scanner/FluentScanner/StepTitleTests.cs
@@ -7,23 +7,32 @@ namespace TestStack.BDDfy.Tests.Scanner.FluentScanner
     [TestFixture]
     public class StepTitleTests
     {
+        private string _mutatedState;
+
         [Test]
         public void MethodCallInStepTitle()
         {
-            var story = this.Given(_ => GivenAValueOf(AMethodCall()))
+            var story = this
+                .Given(_ => GivenWeMutateSomeState())
+                .Then(_ => ThenTitleHas(AMethodCall()))
                 .BDDfy();
 
-            story.Scenarios.Single().Steps.Single().Title.ShouldBe("Given a value of Some value");
+            story.Scenarios.Single().Steps.ElementAt(1).Title.ShouldBe("Then title has Mutated state");
         }
 
         private string AMethodCall()
         {
-            return "Some value";
+            return _mutatedState;
         }
 
-        private void GivenAValueOf(string result)
+        private void GivenWeMutateSomeState()
         {
-            
+            _mutatedState = "Mutated state";
+        }
+
+        private void ThenTitleHas(string result)
+        {
+            result.ShouldBe(_mutatedState);
         }
     }
 }
--- TestStack.BDDfy/Scanners/StepScanners/Fluent/StepArgument.cs
+++ TestStack.BDDfy/Scanners/StepScanners/Fluent/StepArgument.cs
@@ -6,11 +6,12 @@ namespace TestStack.BDDfy
     public class StepArgument
     {
         private readonly Action<object> _set = o => { };
+        private readonly Func<object> _get;
 
         public StepArgument(FieldInfo member, object declaringObject)
         {
             Name = member.Name;
-            Value = member.GetValue(declaringObject);
+            _get = () => member.GetValue(declaringObject);
             _set = o => member.SetValue(declaringObject, o);
             ArgumentType = member.FieldType;
         }
@@ -18,19 +19,33 @@ namespace TestStack.BDDfy
         public StepArgument(PropertyInfo member, object declaringObject)
         {
             Name = member.Name;
-            Value = member.GetGetMethod(true).Invoke(declaringObject, null);
+            _get = () => member.GetGetMethod(true).Invoke(declaringObject, null);
             _set = o => member.GetSetMethod(true).Invoke(declaringObject, new[] { o });
             ArgumentType = member.PropertyType;
         }
 
-        public StepArgument(object value)
+        public StepArgument(Func<object> value)
         {
-            Value = value;
+            _get = value;
             ArgumentType = typeof(object);
         }
 
-        public string Name { get; set; }
-        public object Value { get; set; }
+        public string Name { get; private set; }
+
+        public object Value
+        {
+            get
+            {
+                try
+                {
+                    return _get();
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
         public Type ArgumentType { get; private set; }
 
         public void SetValue(object newValue)
