2f2a3fdf00f9ed83d867afbe9ae99d80a8afa0c1
Jake Ginnivan <jake@ginnivan.net>
2015-03-11 15:23:51 +0000

    Fixed unused variable build error in release mode

--- TestStack.BDDfy.Tests/Exceptions/ExceptionsWhenUsingExamples.cs
+++ TestStack.BDDfy.Tests/Exceptions/ExceptionsWhenUsingExamples.cs
@@ -20,10 +20,9 @@ namespace TestStack.BDDfy.Tests.Exceptions
                 string arg1 = null;
                 string arg2 = null;
                 var arg3 = default(string);
-                var arg4 = default(string);
                 this.Given(_ => AFailingStep(arg1), "Given a failing step")
-                    .Then(_ => NonExecutedStep(arg3), "Then multiple assertions")
-                    .And(_ => NonExecutedStep(arg4), "The second one blows up")
+                    .Then(_ => NonExecutedStep(arg2), "Then multiple assertions")
+                    .And(_ => NonExecutedStep(arg3), "The second one blows up")
                     .WithExamples(exampleTable)
                     .BDDfy();
             });
