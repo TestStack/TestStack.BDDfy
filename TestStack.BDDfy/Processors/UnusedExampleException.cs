2a814782679f3ee83c93befbd0a1e0e3ccaa9151
Jake Ginnivan <jake@ginnivan.net>
2015-03-12 17:00:06 +0000

    Small update to exception suggesting to raise an issue if it is a false positive

--- TestStack.BDDfy/Processors/UnusedExampleException.cs
+++ TestStack.BDDfy/Processors/UnusedExampleException.cs
@@ -7,7 +7,8 @@
     public class UnusedExampleException : Exception
     {
         public UnusedExampleException(ExampleValue unusedValue) :
-            base(string.Format("Example Column '{0}' is unused, all examples should be consumed by the test (have you misspelt a field or property?)", unusedValue.Header))
+            base(string.Format("Example Column '{0}' is unused, all examples should be consumed by the test (have you misspelt a field or property?)\r\n\r\n"
+            + "If this is not the case, raise an issue at https://github.com/TestStack/TestStack.BDDfy/issues.", unusedValue.Header))
         { }
 
         protected UnusedExampleException(
