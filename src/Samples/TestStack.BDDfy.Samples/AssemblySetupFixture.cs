4286834526f248d01a8dc213504f7145783ea33f
Michael Whelan <mjmwdev@gmail.com>
2016-07-01 13:32:23 +0100

    Hooked up AssemblyFixture for xUnit in the Samples project

---
+++ src/Samples/TestStack.BDDfy.Samples/AssemblySetupFixture.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using TestStack.BDDfy.Configuration;
+using TestStack.BDDfy.Reporters.Html;
+using TestStack.BDDfy.Samples;
+using TestStack.BDDfy.Samples.Atm;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+// The custom test framework enables the support
+[assembly: Xunit.TestFramework("TestStack.BDDfy.Samples.XunitTestFrameworkWithAssemblyFixture", "TestStack.BDDfy.Samples")]
+
+// Add one of these for every fixture classes for the assembly.
+// Just like other fixtures, you can implement IDisposable and it'll
+// get cleaned up at the end of the test run.
+[assembly: AssemblyFixture(typeof(AssemblySetupFixture))]
+
+namespace TestStack.BDDfy.Samples
+{
+    public class AssemblySetupFixture 
+    {
+        public AssemblySetupFixture()
+        {
+            Configurator.Processors.Add(() => new CustomTextReporter());
+            Configurator.BatchProcessors.MarkDownReport.Enable();
+            Configurator.BatchProcessors.DiagnosticsReport.Enable();
+            Configurator.BatchProcessors.Add(new HtmlReporter(new AtmHtmlReportConfig(), new MetroReportBuilder()));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class AssemblyFixtureAttribute : Attribute
+    {
+        public AssemblyFixtureAttribute(Type fixtureType)
+        {
+            FixtureType = fixtureType;
+        }
+
+        public Type FixtureType { get; private set; }
+    }
+
+    public class XunitTestFrameworkWithAssemblyFixture : XunitTestFramework
+    {
+        public XunitTestFrameworkWithAssemblyFixture(IMessageSink messageSink)
+            : base(messageSink)
+        { }
+
+        protected override ITestFrameworkExecutor CreateExecutor(AssemblyName assemblyName)
+            => new XunitTestFrameworkExecutorWithAssemblyFixture(assemblyName, SourceInformationProvider, DiagnosticMessageSink);
+    }
+
+    public class XunitTestFrameworkExecutorWithAssemblyFixture : XunitTestFrameworkExecutor
+    {
+        public XunitTestFrameworkExecutorWithAssemblyFixture(AssemblyName assemblyName,
+            ISourceInformationProvider sourceInformationProvider, IMessageSink diagnosticMessageSink)
+            : base(assemblyName, sourceInformationProvider, diagnosticMessageSink)
+        {
+        }
+
+        protected override async void RunTestCases(IEnumerable<IXunitTestCase> testCases,
+            IMessageSink executionMessageSink, ITestFrameworkExecutionOptions executionOptions)
+        {
+            using (
+                var assemblyRunner = new XunitTestAssemblyRunnerWithAssemblyFixture(TestAssembly, testCases,
+                    DiagnosticMessageSink, executionMessageSink, executionOptions))
+                await assemblyRunner.RunAsync();
+        }
+    }
+
+    public class XunitTestAssemblyRunnerWithAssemblyFixture : XunitTestAssemblyRunner
+    {
+        readonly Dictionary<Type, object> assemblyFixtureMappings = new Dictionary<Type, object>();
+
+        public XunitTestAssemblyRunnerWithAssemblyFixture(ITestAssembly testAssembly,
+                                                          IEnumerable<IXunitTestCase> testCases,
+                                                          IMessageSink diagnosticMessageSink,
+                                                          IMessageSink executionMessageSink,
+                                                          ITestFrameworkExecutionOptions executionOptions)
+            : base(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions)
+        { }
+
+        protected override async Task AfterTestAssemblyStartingAsync()
+        {
+            // Let everything initialize
+            await base.AfterTestAssemblyStartingAsync();
+
+            // Go find all the AssemblyFixtureAttributes adorned on the test assembly
+            Aggregator.Run(() =>
+            {
+                var fixturesAttrs = ((IReflectionAssemblyInfo)TestAssembly.Assembly).Assembly
+                                                                                    .GetCustomAttributes(typeof(AssemblyFixtureAttribute))
+                                                                                    .Cast<AssemblyFixtureAttribute>()
+                                                                                    .ToList();
+
+                // Instantiate all the fixtures
+                foreach (var fixtureAttr in fixturesAttrs)
+                    assemblyFixtureMappings[fixtureAttr.FixtureType] = Activator.CreateInstance(fixtureAttr.FixtureType);
+            });
+        }
+
+        protected override Task BeforeTestAssemblyFinishedAsync()
+        {
+            // Make sure we clean up everybody who is disposable, and use Aggregator.Run to isolate Dispose failures
+            foreach (var disposable in assemblyFixtureMappings.Values.OfType<IDisposable>())
+                Aggregator.Run(disposable.Dispose);
+
+            return base.BeforeTestAssemblyFinishedAsync();
+        }
+
+        protected override Task<RunSummary> RunTestCollectionAsync(IMessageBus messageBus,
+                                                                   ITestCollection testCollection,
+                                                                   IEnumerable<IXunitTestCase> testCases,
+                                                                   CancellationTokenSource cancellationTokenSource)
+            => new XunitTestCollectionRunnerWithAssemblyFixture(assemblyFixtureMappings, testCollection, testCases, DiagnosticMessageSink, messageBus, TestCaseOrderer, new ExceptionAggregator(Aggregator), cancellationTokenSource).RunAsync();
+    }
+
+    public class XunitTestCollectionRunnerWithAssemblyFixture : XunitTestCollectionRunner
+    {
+        readonly Dictionary<Type, object> assemblyFixtureMappings;
+        readonly IMessageSink diagnosticMessageSink;
+
+        public XunitTestCollectionRunnerWithAssemblyFixture(Dictionary<Type, object> assemblyFixtureMappings,
+                                                            ITestCollection testCollection,
+                                                            IEnumerable<IXunitTestCase> testCases,
+                                                            IMessageSink diagnosticMessageSink,
+                                                            IMessageBus messageBus,
+                                                            ITestCaseOrderer testCaseOrderer,
+                                                            ExceptionAggregator aggregator,
+                                                            CancellationTokenSource cancellationTokenSource)
+            : base(testCollection, testCases, diagnosticMessageSink, messageBus, testCaseOrderer, aggregator, cancellationTokenSource)
+        {
+            this.assemblyFixtureMappings = assemblyFixtureMappings;
+            this.diagnosticMessageSink = diagnosticMessageSink;
+        }
+
+        protected override Task<RunSummary> RunTestClassAsync(ITestClass testClass, IReflectionTypeInfo @class, IEnumerable<IXunitTestCase> testCases)
+        {
+            // Don't want to use .Concat + .ToDictionary because of the possibility of overriding types,
+            // so instead we'll just let collection fixtures override assembly fixtures.
+            var combinedFixtures = new Dictionary<Type, object>(assemblyFixtureMappings);
+            foreach (var kvp in CollectionFixtureMappings)
+                combinedFixtures[kvp.Key] = kvp.Value;
+
+            // We've done everything we need, so let the built-in types do the rest of the heavy lifting
+            return new XunitTestClassRunner(testClass, @class, testCases, diagnosticMessageSink, MessageBus, TestCaseOrderer, new ExceptionAggregator(Aggregator), CancellationTokenSource, combinedFixtures).RunAsync();
+        }
+    }
+
+}
