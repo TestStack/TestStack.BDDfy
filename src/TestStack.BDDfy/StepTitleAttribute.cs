042ab2cd66b0b6edd97c7d066c9e0bd562855e95
Tom Longhurst <tom.longhurst@asos.com>
2018-12-03 17:46:41 +0000

    Property AutoFormatStepText. When false, will retain capitalisation / formatting of StepTitle, which is otherwise overwritten by Humanize

--- src/TestStack.BDDfy/StepTitleAttribute.cs
+++ src/TestStack.BDDfy/StepTitleAttribute.cs
@@ -4,20 +4,18 @@ namespace TestStack.BDDfy
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class StepTitleAttribute : Attribute
-    {
-        public StepTitleAttribute(string stepTitle)
-        {
-            StepTitle = stepTitle;
-        }
-        
-        public StepTitleAttribute(string stepTitle, bool includeInputsInStepTitle)
+    {        
+        public StepTitleAttribute(string stepTitle, bool includeInputsInStepTitle = false, bool autoFormatStepText = false)
         {
             IncludeInputsInStepTitle = includeInputsInStepTitle;
             StepTitle = stepTitle;
+            AutoFormatStepText = autoFormatStepText;
         }
 
         public string StepTitle { get; private set; }
 
         public bool? IncludeInputsInStepTitle { get; private set; }
+
+        public bool AutoFormatStepText { get; }
     }
 }
