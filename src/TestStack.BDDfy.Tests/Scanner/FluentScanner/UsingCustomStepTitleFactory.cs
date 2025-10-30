873213ed43d97b4e69be52b149cfca89c7da0210
Gurpreet Singh <gps@coding.com>
2025-10-30 23:38:58 +0000

    add step title factory tests

--- src/TestStack.BDDfy.Tests/Scanner/FluentScanner/UsingCustomStepTitleFactory.cs
+++ src/TestStack.BDDfy.Tests/Scanner/FluentScanner/UsingCustomStepTitleFactory.cs
@@ -25,36 +25,49 @@ namespace TestStack.BDDfy.Tests.Scanner.FluentScanner
         [Fact]
         public void ShouldUseCustomStepTitleFactoryWhenSet()
         {
-            var scenario = new UsingCustomStepTitleFactory();
-            var configurator = TestContext.GetContext(scenario).Configurator;
-            configurator.StepTitleFactory = new CustomStepTitleFactory();
+            Configurator.StepTitleFactory = new CustomStepTitleFactory();
 
-            var story = scenario
+            try
+            {
+                var story = new UsingCustomStepTitleFactory()
                 .Given(_ => SomeState())
                 .When(_ => SomethingHappens())
                 .Then(_ => SomeResult())
                 .BDDfy();
 
-            story.Scenarios.Single().Steps.ElementAt(0).Title.ShouldBe("Custom Step Title");
-            story.Scenarios.Single().Steps.ElementAt(1).Title.ShouldBe("Custom Step Title");
-            story.Scenarios.Single().Steps.ElementAt(2).Title.ShouldBe("Custom Step Title");
+                story.Scenarios.Single().Steps.ElementAt(0).Title.ShouldBe("Custom Step Title");
+                story.Scenarios.Single().Steps.ElementAt(1).Title.ShouldBe("Custom Step Title");
+                story.Scenarios.Single().Steps.ElementAt(2).Title.ShouldBe("Custom Step Title");
+            }
+            finally
+            {
+                Configurator.StepTitleFactory = new DefaultStepTitleFactory();
+            }
+
         }
 
         [Fact]
         public void ShouldUseCustomStepTitleFactoryWhenSetWithStepTitles()
         {
-            var scenario = new UsingCustomStepTitleFactory();
-            var configurator = TestContext.GetContext(scenario).Configurator;
-            configurator.StepTitleFactory = new CustomStepTitleFactory();
-            var story = scenario
+            Configurator.StepTitleFactory = new CustomStepTitleFactory();
+
+            try
+            {
+                var story = new UsingCustomStepTitleFactory()
                 .Given(_ => SomeState(), "Not this")
                 .When(_ => SomethingHappens(), "Or this")
                 .Then(_ => SomeResult(), "should not appear")
                 .BDDfy();
 
-            story.Scenarios.Single().Steps.ElementAt(0).Title.ShouldBe("Custom Step Title");
-            story.Scenarios.Single().Steps.ElementAt(1).Title.ShouldBe("Custom Step Title");
-            story.Scenarios.Single().Steps.ElementAt(2).Title.ShouldBe("Custom Step Title");
+                story.Scenarios.Single().Steps.ElementAt(0).Title.ShouldBe("Custom Step Title");
+                story.Scenarios.Single().Steps.ElementAt(1).Title.ShouldBe("Custom Step Title");
+                story.Scenarios.Single().Steps.ElementAt(2).Title.ShouldBe("Custom Step Title");
+            }
+            finally
+            {
+              Configurator.StepTitleFactory = new DefaultStepTitleFactory();
+
+            }
         }
 
         [StepTitle("given from attribute")]
