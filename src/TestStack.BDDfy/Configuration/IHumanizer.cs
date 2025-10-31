6443698543da2cfa18e30fb7d3b66f07a881a379
Gurpreet Singh <gps@coding.com>
2025-10-31 20:26:46 +0000

    remove un used property function

--- src/TestStack.BDDfy/Configuration/IHumanizer.cs
+++ src/TestStack.BDDfy/Configuration/IHumanizer.cs
@@ -4,7 +4,6 @@ namespace TestStack.BDDfy.Configuration
 {
     public interface IHumanizer
     {
-        Func<string, bool> PreserveCasingWhen { get; set; }
         string Humanize(string input);
     }
 }
