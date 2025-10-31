4e471e3098d1f9fc4c08df9fb3eb2e11dcf16bb7
Gurpreet Singh <gps@coding.com>
2025-10-31 20:50:21 +0000

    cleanup

--- src/TestStack.BDDfy/DefaultHumanizer.cs
+++ src/TestStack.BDDfy/DefaultHumanizer.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.VisualBasic;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System;
 using System.Linq;
 using System.Text.RegularExpressions;
 using TestStack.BDDfy.Configuration;
@@ -10,32 +7,38 @@ namespace TestStack.BDDfy
 {
     internal partial class DefaultHumanizer: IHumanizer
     {
-        private static readonly TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        private static readonly Regex ConsecutiveCapitalLetters = new("([A-Z]+)([A-Z][a-z])");
+        private static readonly Regex TokensPattern = new("__([a-zA-Z0-9]+)__");
+        private static readonly Regex PascalCaseRegex = new(@"(?<!^)([A-Z])");
+        private static readonly Regex TokenReplacePattern = new("#(\\S+)#");
+        private static readonly Regex SentenceWithNumerals = new(@"(?<=[a-zA-Z])(?=\d)(?![^<>]*>)");
+        private static readonly Regex UnicodeMatchPattern = new(@"[^\u0000-\u007F]");
+        private static readonly Regex LoneIReplacePattern = new(@"(?<=^|\s)i(?=\s|$)");
 
         public string Humanize(string input)
         {
             var shouldPreserveCasing = input.Replace("__", "-").Contains('_');
 
-            input = TokensPattern().Replace(input, "-#$1#-");
+            input = TokensPattern.Replace(input, "-#$1#-");
 
             var words = input.Split(['_','-']);
 
-            var finalWords = words.Select(x => TokenReplacePattern().Replace(x, "<$1>"));
+            var finalWords = words.Select(x => TokenReplacePattern.Replace(x, "<$1>"));
 
             var sentence = string.Join(" ", finalWords);
 
             if (!shouldPreserveCasing)
             {
-                sentence = ConsecutiveCapitalLetters().Replace(sentence, "$1 $2");
+                sentence = ConsecutiveCapitalLetters.Replace(sentence, "$1 $2");
                 sentence = PascalToSentence(sentence);
-                sentence = SentenceWithNumerals().Replace(sentence, " ");
-                if (UnicodeMatchPattern().IsMatch(input)) 
+                sentence = SentenceWithNumerals.Replace(sentence, " ");
+                if (UnicodeMatchPattern.IsMatch(input)) 
                     throw new ArgumentException("Non ascii characters detected");
             }
             
             
             sentence = sentence.Trim().Replace("  "," ");
-            sentence = LoneIReplacePattern().Replace(sentence, "I");
+            sentence = LoneIReplacePattern.Replace(sentence, "I");
             return sentence;
         }
 
@@ -43,32 +46,10 @@ namespace TestStack.BDDfy
         {
             if (string.IsNullOrEmpty(input)) return input;
 
-            var sentence = PascalCaseRegex().Replace(input, " $1");
+            var sentence = PascalCaseRegex.Replace(input, " $1");
             sentence = sentence.Replace("< ", "<").Replace(" >",">");
-            var final = char.ToUpper(sentence[0]) + sentence.Substring(1).ToLower();
+            var final = char.ToUpper(sentence[0]) + sentence[1..].ToLower();
             return final;
         }
-
-
-        [GeneratedRegex("([A-Z]+)([A-Z][a-z])")]
-        private static partial Regex ConsecutiveCapitalLetters();
-
-        [GeneratedRegex("__([a-zA-Z0-9]+)__")]
-        private static partial Regex TokensPattern();
-
-        [GeneratedRegex(@"(?<!^)([A-Z])")]
-        private static partial Regex PascalCaseRegex();
-
-        [GeneratedRegex("#(\\S+)#")]
-        private static partial Regex TokenReplacePattern();
-
-        [GeneratedRegex(@"(?<=[a-zA-Z])(?=\d)(?![^<>]*>)")]
-        private static partial Regex SentenceWithNumerals();
-
-        [GeneratedRegex(@"[^\u0000-\u007F]")]
-        private static partial Regex UnicodeMatchPattern();
-
-        [GeneratedRegex(@"(?<=^|\s)i(?=\s|$)")]
-        private static partial Regex LoneIReplacePattern();
     }
 }
--- src/TestStack.BDDfy/Processors/UnusedExampleException.cs
+++ src/TestStack.BDDfy/Processors/UnusedExampleException.cs
@@ -2,12 +2,13 @@
 
 namespace TestStack.BDDfy.Processors
 {
- [Serializable]
- public class UnusedExampleException : Exception
- {
- public UnusedExampleException(ExampleValue unusedValue) : base(string.Format("Example Column '{0}' is unused, all examples should be consumed by the test (have you misspelt a field or property?)\r\n\r\n"
- + "If this is not the case, raise an issue at https://github.com/TestStack/TestStack.BDDfy/issues.", unusedValue.Header))
- {
- }
- }
+    [Serializable]
+    public class UnusedExampleException : Exception
+    {
+        public UnusedExampleException(ExampleValue unusedValue) 
+            : base(string.Format("Example Column '{0}' is unused, all examples should be consumed by the test (have you misspelt a field or property?)\r\n\r\n"
+        + "If this is not the case, raise an issue at https://github.com/TestStack/TestStack.BDDfy/issues.", unusedValue.Header))
+        {
+        }
+    }
 }
