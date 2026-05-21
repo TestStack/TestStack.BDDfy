0adb5f0d6400c455f15c843e1ea37750fcce02f4
Gurpreet Singh <gps@coding.com>
2026-05-21 23:01:41 +0100

    use startup instead of setup fixture

---
+++ src/Samples/TestStack.BDDfy.Samples/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using TestStack.BDDfy.Configuration;
+using TestStack.BDDfy.Reporters.Html;
+using TestStack.BDDfy.Samples.Atm;
+
+namespace TestStack.BDDfy.Samples
+{
+    public class Startup
+    {
+        [ModuleInitializer]
+        public static void Initialize()
+        {
+            Configurator.Processors.Add(() => new CustomTextReporter());
+            Configurator.Processors.ConsoleReport.Enable();
+            Configurator.BatchProcessors.MarkDownReport.Enable();
+            Configurator.BatchProcessors.DiagnosticsReport.Enable();
+            Configurator.BatchProcessors.Add(new HtmlReporter(new AtmHtmlReportConfig(), new MetroReportBuilder()));
+        }
+    }
+}
