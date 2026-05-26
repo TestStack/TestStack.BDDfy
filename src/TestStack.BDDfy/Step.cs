896582d16319e0fce1cf03665f1c91f568cff952
Gurpreet Singh <gps@coding.com>
2026-05-26 12:18:16 +0100

    use implicit string conversion

--- src/TestStack.BDDfy/Step.cs
+++ src/TestStack.BDDfy/Step.cs
@@ -42,7 +42,7 @@ namespace TestStack.BDDfy
         internal Func<object, object> Action { get; set; }
         public bool Asserts { get; private set; }
         public bool ShouldReport { get; private set; }
-        public string Title => _title??= _stepTitle.ToString();
+        public string Title => _title??= _stepTitle;
         public ExecutionOrder ExecutionOrder { get; private set; }
         public Result Result { get; set; }
         public Exception Exception { get; set; }
