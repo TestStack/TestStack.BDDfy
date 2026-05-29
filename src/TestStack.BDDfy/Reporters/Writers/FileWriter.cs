f626b20b44d85bcb6f1a0153fd703e0e66f27cce
Gurpreet Singh <dear_gurpreet@hotmail.com>
2026-05-29 11:32:45 +0100

    make FileWriter more flexible with paths (#303)
    * Made FileWriter more flexible with paths and now it will create path if it does not exist
    
    * disable copilot code review job untill we get copilot access

--- src/TestStack.BDDfy/Reporters/Writers/FileWriter.cs
+++ src/TestStack.BDDfy/Reporters/Writers/FileWriter.cs
@@ -1,17 +1,18 @@
+using System;
 using System.IO;
 
 namespace TestStack.BDDfy.Reporters.Writers
 {
     public class FileWriter : IReportWriter
     {
-        public void OutputReport(string reportData, string reportName, string outputDirectory = null)
+        public void OutputReport(string reportData, string reportName, string? outputDirectory = null)
         {
-            string directory = outputDirectory ?? FileHelpers.AssemblyDirectory();
-            var path = Path.Combine(directory, reportName);
+            var filePath = Path.Combine(outputDirectory ?? FileHelpers.AssemblyDirectory(), reportName);
+            string directory = Path.GetDirectoryName(filePath) ?? throw new InvalidOperationException("Unable to determine directory.");
 
-            if (File.Exists(path))
-                File.Delete(path);
-            File.WriteAllText(path, reportData);
+            Directory.CreateDirectory(directory);
+
+            File.WriteAllText(filePath, reportData);
         }
     }
 }
