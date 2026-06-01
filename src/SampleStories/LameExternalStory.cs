821bd319230ebec5decec97b0b7f9de1431183f5
Gurpreet Singh <dear_gurpreet@hotmail.com>
2026-06-01 11:55:47 +0100

    added tests for loading stories from different assembly than the test assembly (#311)
    Co-authored-by: Gurpreet Singh <gurpreet.singh@asos.com>

---
+++ src/SampleStories/LameExternalStory.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace SampleStories
+{
+    public class LameExternalStory
+    {
+        int state = 0;
+        public void GivenANumberThree() => state = 3;
+        public void WhenIMultiplyTheNumberByItsef() => state *= state;
+        public void ThenTheNumberIsSquared() => Assert.Equal(9, state);
+    }
+}
