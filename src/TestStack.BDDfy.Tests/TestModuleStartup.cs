c4852710c65acb9faf49167052e8a1a6e11b3493
Gurpreet Singh <gurpreet.singh@asos.com>
2026-06-02 09:36:31 +0100

    configure incoming approvals int output directory to prevent race condition to delete those after test

--- src/TestStack.BDDfy.Tests/TestModuleStartup.cs
+++ src/TestStack.BDDfy.Tests/TestModuleStartup.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Shouldly;
 using TestStack.BDDfy.Configuration;
 
 namespace TestStack.BDDfy.Tests
@@ -10,6 +12,21 @@ namespace TestStack.BDDfy.Tests
         {
             Configurator.Processors.Add(() => new XUnitOutputReporter());
             Configurator.Processors.ConsoleReport.Enable();
+
+            ShouldlyConfiguration.ShouldMatchApprovedDefaults.WithFilenameGenerator((testMethodInfo, discriminator, type, extension) =>
+            {
+                var tfm = $"net{Environment.Version.Major}.{Environment.Version.Minor}";
+                var receivedFolder = Path.Combine(AppContext.BaseDirectory, "ApprovalTests");
+                Directory.CreateDirectory(receivedFolder);
+
+                var baseName = $"{testMethodInfo.DeclaringTypeName}.{testMethodInfo.MethodName}";
+                if (!string.IsNullOrEmpty(discriminator))
+                    baseName += $"_{discriminator}";
+
+                return type == "received"
+                    ? Path.Combine(receivedFolder, $"{baseName}.{type}.{extension}")
+                    : $"{testMethodInfo.SourceFileDirectory}{Path.DirectorySeparatorChar}{baseName}.{type}.{extension}";
+            });
         }
     }
 }
